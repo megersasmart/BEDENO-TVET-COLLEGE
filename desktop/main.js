ca4087916d64a79aaabc34bc92ea4aef5c203d81
Megersa Smart <gadaacomputer@gmail.com>
2026-09-11 23:24:43 +0300

    Update main.js

--- desktop/main.js
+++ desktop/main.js
@@ -1,1 +1,32 @@
+const { app, BrowserWindow } = require("electron");
 
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    minWidth: 900,
+    minHeight: 600,
+    webPreferences: {
+      contextIsolation: true,
+      nodeIntegration: false
+    }
+  });
+
+  win.loadURL("https://bedeno-tvet-college.onrender.com");
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
